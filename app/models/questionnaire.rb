6b518ad15e28c2417692dc366a40e5a31501d8bb
Yuki Inoue <inoue.yuki@moneyforward.co.jp>
2020-05-18 14:07:17 +0900

    set Tariff in config/initializers/constants.rb

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -3,7 +3,6 @@ class Questionnaire < ApplicationRecord
 
   def qol_score
     health_state = 10000 * mobility + 1000 * self_care + 100 * usual_activities + 10 * pain + anxiety
-    tariff = eval File.read( File.expand_path( "../hash_file/tariff.rb", __FILE__ ))
-    tariff[health_state]
+    QolValues::Tariff[health_state]
   end
 end
